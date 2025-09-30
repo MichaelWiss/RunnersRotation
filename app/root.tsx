b75730f4d4e435e7df232e0c69d35630a630ab7f
MichaelWiss <michael.wiss@gmail.com>
2025-09-30 14:50:00 -0500

    hero spacing

--- app/root.tsx
+++ app/root.tsx
@@ -148,53 +148,113 @@ const CART_QUERY = `#graphql
   query CartQuery($cartId: ID!, $country: CountryCode, $language: LanguageCode)
   @inContext(country: $country, language: $language) {
     cart(id: $cartId) {
-      id
-      checkoutUrl
-      totalQuantity
-      cost {
-        subtotalAmount {
-          amount
-          currencyCode
-        }
-        totalAmount {
-          amount
-          currencyCode
-        }
+      ...CartFragment
+    }
+  }
+
+  fragment CartFragment on Cart {
+    id
+    checkoutUrl
+    totalQuantity
+    buyerIdentity {
+      countryCode
+      customer {
+        id
+        email
+        firstName
+        lastName
+        displayName
       }
-      lines(first: 100) {
-        edges {
-          node {
-            id
-            quantity
-            cost {
-              totalAmount {
-                amount
-                currencyCode
-              }
+      email
+      phone
+    }
+    lines(first: 100) {
+      edges {
+        node {
+          id
+          quantity
+          attributes {
+            key
+            value
+          }
+          cost {
+            totalAmount {
+              amount
+              currencyCode
             }
-            merchandise {
-              ... on ProductVariant {
-                id
+            amountPerQuantity {
+              amount
+              currencyCode
+            }
+            compareAtAmountPerQuantity {
+              amount
+              currencyCode
+            }
+          }
+          merchandise {
+            ... on ProductVariant {
+              id
+              availableForSale
+              compareAtPrice {
+                ...MoneyFragment
+              }
+              price {
+                ...MoneyFragment
+              }
+              requiresShipping
+              title
+              image {
+                ...ImageFragment
+              }
+              product {
+                handle
                 title
-                availableForSale
-                price {
-                  amount
-                  currencyCode
-                }
-                product {
-                  title
-                  handle
-                }
-                image {
-                  url
-                  altText
-                }
+                id
+              }
+              selectedOptions {
+                name
+                value
               }
             }
           }
         }
       }
     }
+    cost {
+      subtotalAmount {
+        ...MoneyFragment
+      }
+      totalAmount {
+        ...MoneyFragment
+      }
+      totalDutyAmount {
+        ...MoneyFragment
+      }
+      totalTaxAmount {
+        ...MoneyFragment
+      }
+    }
+    note
+    attributes {
+      key
+      value
+    }
+    discountCodes {
+      code
+    }
+  }
+
+  fragment MoneyFragment on MoneyV2 {
+    currencyCode
+    amount
+  }
+
+  fragment ImageFragment on Image {
+    id
+    url
+    altText
+    width
+    height
   }
 `;
 

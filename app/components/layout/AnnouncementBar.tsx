f1d2b812d43bbfbfdaacc8ebf70e769770f39b44
MichaelWiss <michael.wiss@gmail.com>
2025-10-15 11:18:07 -0500

    animated scrolling marquee for announcement bar

--- app/components/layout/AnnouncementBar.tsx
+++ app/components/layout/AnnouncementBar.tsx
@@ -1,7 +1,16 @@
 export default function AnnouncementBar() {
   return (
     <div className="announcement-bar full-width">
-      Free shipping on orders over $150 (US) | Use code RUNFREE at checkout
+      <div className="marquee">
+        <div className="marquee-content">
+          <span>Free Shipping on Orders over $150</span>
+          <span>All Duties and Taxes Paid</span>
+          <span>Free and Easy Returns</span>
+          <span>Free Shipping on Orders over $150</span>
+          <span>All Duties and Taxes Paid</span>
+          <span>Free and Easy Returns</span>
+        </div>
+      </div>
     </div>
   );
 }
